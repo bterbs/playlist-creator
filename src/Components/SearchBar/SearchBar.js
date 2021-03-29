d59cf4f9d2572f6c97503bcc104a367794ba7b5e
Brittany <brterbush@zappos.com>
2021-03-28 17:57:42 -0700

    SearchBar should be functional component.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,15 +1,13 @@
 import { Component } from "react";
 import "./SearchBar.css";
 
-class SearchBar extends Component {
-  render() {
-    return (
-      <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton">SEARCH</button>
-      </div>
-    );
-  }
+function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <button className="SearchButton">SEARCH</button>
+    </div>
+  );
 }
 
 export default SearchBar;
